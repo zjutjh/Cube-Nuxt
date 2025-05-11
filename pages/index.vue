d8461ddfaecfed9152018ec0b09b0a59e39cb76b
折乙 <zheyi563@gmail.com>
2025-05-11 11:34:40 +0800

    feat: 分页配置完成

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { useRouter } from "vue-router";
+const router = useRouter();
+onMounted(() => {
+  if (localStorage.getItem("key")) {
+    router.push("/home");
+  } else {
+    router.push("/login");
+  }
+});
+</script>
---
+++ pages/login/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="login">
+    <h1>登录</h1>
+  </div>
+</template>
+<script setup lang="ts">
+
+</script>
