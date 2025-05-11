49829d5da3957ce494c09a628ef31d1eac2bbc06
折乙 <zheyi563@gmail.com>
2025-05-11 10:52:55 +0800

    chore: 安装@element-plus/nuxt依赖, 配置elment-plus

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon']
-})
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/icon",
+    "@element-plus/nuxt"
+  ],
+  css: [
+    "element-plus/dist/index.css"
+  ]
+});
