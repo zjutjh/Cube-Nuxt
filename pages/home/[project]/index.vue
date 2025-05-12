eab3074981db63ead895f45563bc85a9ac660637
折乙 <zheyi563@gmail.com>
2025-05-12 11:44:16 +0800

    feat(home) 基本框架

---
+++ pages/home/[project]/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">
+    <h1>{{ route.params.project }}</h1>
+  </div>
+</template>
+<script setup lang="ts">
+import "../index.scss";
+const route = useRoute();
+
+
+</script>
