4673c87f66670e5c22e4b057a6d3eef81c4e6e48
折乙 <zheyi563@gmail.com>
2025-05-12 20:54:41 +0800

    chore: 配跨域, vue-query依赖补全

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,16 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxt/eslint", "@nuxt/icon", "@element-plus/nuxt"],
-  css: ["element-plus/dist/index.css"]
+  css: ["element-plus/dist/index.css"],
+  vite: {
+    server: {
+      proxy: {
+        "/ap": {
+          target: "http://101.126.88.138:8083",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, "")
+        }
+      }
+    }
+  }
 });
